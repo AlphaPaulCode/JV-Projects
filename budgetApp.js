1a6a3a63cc8691c20dbc3f5cef33795678f0eca2
Erioro Paul <erioroegbelughepaul@gmail.com>
2023-11-16 13:35:36 +0100

    new

---
+++ budgetApp.js
@@ -0,0 +1,23 @@
+const balanceEl = document.querySelector(".balance .value");
+const incomeTotalEl = document.querySelector(".incomeTotal");
+const outcomeTotalEl = document.querySelector(".outcomeTotal");
+const incomeEl = document.querySelector("#income");
+const expenseEl = document.querySelector("#expense");
+const allEl = document.querySelector("#all");
+const incomeList = document.querySelector("#income .list");
+const expenseList = document.querySelector("#outcome .list");
+const allList = document.querySelector("#all .list");
+
+//select Btns
+const expenseBtn = document.querySelector(".tab1");
+const incomeBtn = document.querySelector(".tab2");
+const allBn = document.querySelector(".tab3");
+
+//input Bts
+const addExpense = document.querySelector(".addExpense");
+const expenseTitle = document.querySelector("expenseTitleInput");
+const expenseAmount = document.querySelector("expenseAmountInput");
+
+const addIncome = document.querySelector(".addIncome");
+const incomeTitle = document.querySelector("incomeTitleInput");
+const incomeAmount = document.querySelector("incomeAmountInput");
